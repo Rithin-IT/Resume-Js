23b8f5d3b6e5d8a6c84a5c3fddd92a21a73edf4c
Rithin-IT <rithinrock15@gmail.com>
2024-11-27 10:52:11 +0530

    resume-js

---
+++ main.js
@@ -0,0 +1,70 @@
+let resume={
+    name:"Anto Rithin",
+    contact:"9360653380",
+    email:"rithinanto@gmail.com",
+
+    objective:"Creative and driven Software Developer with a passion for building efficient and scalable applications.",
+
+    education:[
+    {
+    institute_name: "Indian Institute of Engineers",
+    course_name:"Civil Engineering",
+    Year:2019,
+    },
+    {
+    institute_name: "Carmel Higher Secondary School",
+    course_name:"HSC",
+    year:2015,
+    },
+    {
+    institute_name: "st.tresas matriculation school",
+    course_name:"SSLC",
+    year:2013,
+    },
+    ],
+
+    project_name:[
+        "Risk Management in Construction Management"
+    ],
+
+    work_experience:{
+    company_name:"Jeyasekharan Medical Trust",
+    designation:"Insurance executive",
+    experience:"One Year",
+    },
+
+    certification:[
+    {
+    academy:"Agaram software academy",
+    course_name:"full stack developer",
+    contact_number:"09444074214",
+    },
+    {
+    academy:"Cadd",
+    course_name:"Tally erp 9",
+    contact_number:"9249596921",
+    },
+    ],
+    skills:[
+    "Microsoft_Excel",
+    "Microsoft_Word",
+    "Photoshop",
+    "Revit architecture",
+    "Autodesk autocad",
+    ],
+    languages_known:[
+    "Tamil",
+    "Hindi",
+    "English",
+    "Malayalam",
+    ],
+    personal_details:{
+    father_name:"S Raja Retnam",
+    mother_name:"Y Sahaya Remilal",
+    dob: 14-2-1997,
+    hobbies:["Riding Bike",
+            "Gardening", 
+            "web series",],
+    personal_interest:["Reading Books"],
+    },
+  }
